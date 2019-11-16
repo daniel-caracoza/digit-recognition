1395238ba2949cd0068f6a0c7489053158382c2f
daniel-caracoza <daniel.caracoza@bellevuecollege.edu>
2019-11-15 21:45:24 -0800

    Initial commit

---
+++ ann.py
@@ -0,0 +1,11 @@
+"""
+Daniel Caracoza
+Machine Learning Fall 2019
+Assignment 3 Artificial Neural Networks - Backpropagation Algorithm
+"""
+import pandas as pd
+import numpy as np
+
+
+def create_network(n_inputs, n_hidden, n_outputs):
+    a = 3
